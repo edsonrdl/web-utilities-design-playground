5e8291bbaf99b5611629d0514c67e3b715e28c79
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-11-29 22:53:09 -0300

    refactor:Corrigido script

--- imagem effect/selecionar imagem aleatoria e exibir/script.js
+++ imagem effect/selecionar imagem aleatoria e exibir/script.js
@@ -1,62 +1,94 @@
 function getRandomIcon() {
-  
-    const imgsArray = [
-      "./img/icon-.net-framework.png",
-      "./img/icon-angular.png",
-      "./img/icon-c-sharp-logo.png",
-      "./img/icon-css.png",
-      "./img/icon-docker.png",
-      "./img/icon-html-5.png",
-      "./img/icon-java.png",
-      "./img/icon-javascript.png",
-      "./img/icon-mongodb.png",
-      "./img/icon-mysql.png",
-      "./img/icon-node-js.png",
-      "./img/icon-postgres.png",
-      "./img/icon-postman.png",
-      "./img/icon-python.png",
-      "./img/icon-sass.png",
-      "./img/icon-spring-boot.png",
-      "./img/icon-sql-server.png",
-      "./img/icon-typescript.png"
-    ];
-  
-  
-    return imgsArray[Math.floor(Math.random() * imgsArray.length)];
+
+  const imgsArray = [
+    "../imagens/icons/icon-.net-framework.png",
+    "../imagens/icons/icon-angula.png",
+    "../imagens/icons/icon-c-sharp-logo.png",
+    "../imagens/icons/icon-css.png",
+    "../imagens/icons/icon-docker.png",
+    "../imagens/icons/icon-html-5.png",
+    "../imagens/icons/icon-java.png",
+    "../imagens/icons/icon-javascript.png",
+    "../imagens/icons/icon-mongodb.png",
+    "../imagens/icons/icon-mysql.png",
+    "../imagens/icons/icon-node-js.png",
+    "../imagens/icons/icon-postgres.png",
+    "../imagens/icons/icon-postman.png",
+    "../imagens/icons/icon-python.png",
+    "../imagens/icons/icon-sass.png",
+    "../imagens/icons/icon-spring-boot.png",
+    "../imagens/icons/icon-sql-server.png",
+    "../imagens/icons/icon-typescript.png"
+  ];
+
+
+  return imgsArray[Math.floor(Math.random() * imgsArray.length)];
+};
+
+
+function getRandomPosition() {
+  const container = document.getElementById('container-exibi-icons');
+  const widthContainer = container.clientWidth;
+  const heightContainer = container.clientHeight;
+  const width = widthContainer - 100;
+  const height = heightContainer - 100;
+  const x = Math.floor(Math.random() * width);
+  const y = Math.floor(Math.random() * height);
+  return { x, y };
+}
+
+function showIcon() {
+  const imgSrc = getRandomIcon();
+  const container = document.getElementById('container-exibi-icons');
+
+  const imgElement = document.createElement('img');
+  imgElement.className = 'icon';
+  imgElement.setAttribute('src', imgSrc);
+
+  const position = getRandomPosition();
+  imgElement.style.left = position.x + 'px';
+  imgElement.style.top = position.y + 'px';
+  imgElement.style.visibility = 'visible';
+
+  container.appendChild(imgElement);
+
+  setTimeout(() => {
+    imgElement.style.visibility = 'hidden';
+  }, 2000);
+}
+
+function activateFunction() {
+
+  intervalId = setInterval(showIcon, 2000);
+}
+
+function deactivateFunction() {
+
+  clearInterval(intervalId);
+  console.log("Função desativada");
+}
+
+function checkVisibility() {
+  const myDiv = document.getElementById("container-exibi-icons");
+  const boundingBox = myDiv.getBoundingClientRect();
+
+
+  if (
+    boundingBox.top >= 0 &&
+    boundingBox.left >= 0 &&
+    boundingBox.bottom <= window.innerHeight &&
+    boundingBox.right <= window.innerWidth
+  ) {
+
+    activateFunction();
+  } else {
+
+    deactivateFunction();
   }
-  
-  function getRandomPosition() {
-    const container = document.getElementById('icon-container');
-    const width = container.clientWidth;
-    const height = container.clientHeight;
-    const x = Math.floor(Math.random() * width);
-    const y = Math.floor(Math.random() * height);
-    return { x, y };
-  }
-  
-  function showIcon() {
-    const imgSrc = getRandomIcon();
-    const container = document.getElementById('icon-container');
-  
-  
-    const imgElement = document.createElement('img');
-    imgElement.className = 'icon';
-    imgElement.setAttribute('src', imgSrc);
-  
-  
-    const position = getRandomPosition();
-    imgElement.style.left = position.x + 'px';
-    imgElement.style.top = position.y + 'px';
-    imgElement.style.visibility = 'visible';
-  
-  
-    container.appendChild(imgElement);
-  
-  
-    setTimeout(() => {
-      imgElement.style.visibility = 'hidden';
-    }, 2000);
-  }
-  
-  setInterval(showIcon, 2000);
-  
+}
+
+
+document.getElementById("container-exibi-icons").addEventListener("mouseover", checkVisibility);
+document.getElementById("container-exibi-icons").addEventListener("mouseout", deactivateFunction);
+window.addEventListener("scroll", checkVisibility);
+window.addEventListener("resize", checkVisibility);
